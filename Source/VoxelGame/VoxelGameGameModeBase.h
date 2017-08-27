1536ae091d9294e90c1c88c4397c8d13f2f07aaa
connoreccleston <connoreccleston@gmail.com>
2017-08-27 06:13:40 -0600

    Initial commit.

---
+++ Source/VoxelGame/VoxelGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoxelGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOXELGAME_API AVoxelGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
